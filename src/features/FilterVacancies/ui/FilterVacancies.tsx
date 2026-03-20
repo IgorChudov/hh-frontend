f86bd3cc772e33cc3671c56a76e35d575c551471
Igor Chudov <ichudov3@gmail.com>
2026-03-20 11:17:47 +0500

    fix styles

--- src/features/FilterVacancies/ui/FilterVacancies.tsx
+++ src/features/FilterVacancies/ui/FilterVacancies.tsx
@@ -1,4 +1,4 @@
-import { Select } from "@mantine/core";
+import { Select, Card } from "@mantine/core";
 import { useAppDispatch } from "../../../shared/hooks";
 import { setCity, loadVacancies, setPage } from "../../../entities/vacancies/model/vacanciesSlice";
 import LocationIcon from "../../../shared/assets/location-icon.svg?react";
@@ -14,13 +14,16 @@ export const FilterVacancies = () => {
   };
 
   return (
-    <Select
+    <Card className={classes.card}>
+      <Select
       placeholder="Все города"
       data={["Все города", "Москва", "Санкт-Петербург"]}
       onChange={handleCityChange}
       className={classes.select}
       leftSection={<LocationIcon />}
       comboboxProps={{ shadow: "md" }}
-    />
+      />
+    </Card>
+    
   );
 };
